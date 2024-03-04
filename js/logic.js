a6fcd9870f766d4761da4ac1005d554e792d696b
Rezovos <lenzibry@gmail.com>
2024-03-04 13:19:38 -0800

    var dom elements

---
+++ js/logic.js
@@ -0,0 +1,7 @@
+var questionsEl = document.getElementById('questions');
+var timerEl = document.getElementById('time');
+var choicesEl = document.getElementById('choices');
+var submitBtn = document.getElementById('submit');
+var startBtn = document.getElementById('start');
+var initialsEl = document.getElementById('initials');
+var feedbackEl = document.getElementById('feedback');
